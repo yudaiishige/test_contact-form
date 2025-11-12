9004a3ee448fb6a7f7a7f973d93c0a729c48f77b
yudai ishige <daidai.yudai827@gmail.com>
2025-11-12 22:38:40 +0900

    変更

--- src/resources/views/index.blade.php
+++ src/resources/views/index.blade.php
@@ -5,7 +5,7 @@
   <meta charset="UTF-8" />
   <meta http-equiv="X-UA-Compatible" content="IE=edge" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <title>FashionablyLATE</title>
+  <title>FashionablyLate</title>
   <link rel="stylesheet" href="{{ asset('css/sanitize.css') }}" />
   <link rel="stylesheet" href="{{ asset('css/index.css') }}" />
 </head>
@@ -14,7 +14,7 @@
   <header class="header">
     <div class="header__inner">
       <a class="header__logo" href="/">
-        FashionablyLATE
+        FashionablyLate
       </a>
     </div>
   </header>
